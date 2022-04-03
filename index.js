37c209f09a843dc708dab5e7959b33f9d10f1921
DILIP KUMAR JAKHAR <jakhardilip10@gmail.com>
2022-04-03 17:40:10 +0530

    pagination updated

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 
+    let start;
+    let end;
     let url='https://geektrust.s3-ap-southeast-1.amazonaws.com/adminui-problem/members.json';
 
     let appended=document.getElementById("data");
@@ -15,11 +17,11 @@
     }
 
 
-
+    
     function getdata(i){
-        // console.log(i)
+      
         i.map((item)=>{
-
+            //    if(item.id<=10){
          let tr=document.createElement('tr');
          let td_1=document.createElement('td');
          let td_2=document.createElement('td');
@@ -44,17 +46,73 @@
 
     
          span1.addEventListener('click',()=>{
-             Delete(item.id)}
-             );
+             Delete(item.id)});
+             
+             
+             tr.append(td_1,td_2,td_3,td_4,span,span1);
+             appended.append(tr);
+            }
+        
+            )
 
-         tr.append(td_1,td_2,td_3,td_4,span,span1);
-         appended.append(tr);
-        })
-    
-    function getdata1(i){
+            let span_prev2=document.createElement('span');
+            span_prev2.innerHTML=`<button><i class="uil uil-angle-double-left"></i></button>`;
+            span_prev2.addEventListener('click',()=>{
+                handelChange(-4)
+            })
+            appended.append(span_prev2);
+            
+            let span_prev1=document.createElement('span');
+            span_prev1.innerHTML=`<button><i class="uil uil-angle-left"></i></button>`;
+            span_prev2.addEventListener('click',()=>{
+                handelChange(-2)
+            })
+            appended.append(span_prev1);
+
+            let span_num1=document.createElement('span');
+            span_num1.innerHTML=`<button>1</button>`;
+            span_num1.addEventListener('click',()=>{
+                handelChange(1)
+            })
+            appended.append(span_num1);
+
+            let span_num2=document.createElement('span');
+            span_num2.innerHTML=`<button>2</button>`;
+            span_num2.addEventListener('click',()=>{
+                handelChange(2)
+            })
+            appended.append(span_num2);
+            
+            let span_num3=document.createElement('span');
+            span_num3.innerHTML=`<button>3</button>`;
+            span_num3.addEventListener('click',()=>{
+                handelChange(3)
+            })
+            appended.append(span_num3);
+            
+            let span_next1=document.createElement('span');
+            span_next1.innerHTML=`<button><i class="uil uil-angle-right"></i></button>`;
+            span_next1.addEventListener('click',()=>{
+                handelChange(4)
+            })
+            appended.append(span_next1);
+
+            let span_next2=document.createElement('span');
+            span_next2.innerHTML=`<button><i class="uil uil-angle-double-right"></i></button>`;
+            span_next2.addEventListener('click',()=>{
+                handelChange(5)
+            })
+            appended.append(span_next2);
+        
+        }
+
+
+
+    function getdata1(i,start,end){
         appended.innerHTML="";
-        i.map((item)=>{
-
+        console.log(i.length)
+        i.map((item,start,end)=>{
+            // if(item.id-1>=start && item.id-1<=end){
          let tr=document.createElement('tr');
          let td_1=document.createElement('td');
          let td_2=document.createElement('td');
@@ -79,13 +137,67 @@
 
     
          span1.addEventListener('click',()=>{
-             Delete(item.id)}
-             );
+             Delete(item.id)
+            });
 
-         tr.append(td_1,td_2,td_3,td_4,span,span1);
-         appended.append(tr);
+            
+            tr.append(td_1,td_2,td_3,td_4,span,span1);
+            appended.append(tr);
+        // }
         })
-    
+        let span_prev2=document.createElement('span');
+        span_prev2.innerHTML=`<button><i class="uil uil-angle-double-left"></i></button>`;
+        span_prev2.addEventListener('click',()=>{
+            handelChange(-4)
+        })
+        appended.append(span_prev2);
+        
+        let span_prev1=document.createElement('span');
+        span_prev1.innerHTML=`<button><i class="uil uil-angle-left"></i></button>`;
+        span_prev2.addEventListener('click',()=>{
+            handelChange(-2)
+        })
+        appended.append(span_prev1);
+
+        let span_num1=document.createElement('span');
+        span_num1.innerHTML=`<button>1</button>`;
+        span_num1.addEventListener('click',()=>{
+            handelChange(1)
+        })
+        appended.append(span_num1);
+
+        let span_num2=document.createElement('span');
+        span_num2.innerHTML=`<button>2</button>`;
+        span_num2.addEventListener('click',()=>{
+            handelChange(2)
+        })
+        appended.append(span_num2);
+
+        let span_num3=document.createElement('span');
+        span_num3.innerHTML=`<button>3</button>`;
+        span_num3.addEventListener('click',()=>{
+            handelChange(3)
+        })
+        appended.append(span_num3);
+
+        let span_next1=document.createElement('span');
+        span_next1.innerHTML=`<button><i class="uil uil-angle-right"></i></button>`;
+        span_next1.addEventListener('click',()=>{
+            handelChange(4)
+        })
+        appended.append(span_next1);
+
+        let span_next2=document.createElement('span');
+        span_next2.innerHTML=`<button><i class="uil uil-angle-double-right"></i></button>`;
+        span_next2.addEventListener('click',()=>{
+            handelChange(5)
+        })
+
+        appended.append(span_next2);
+
+        let btn=document.createElement('button');
+        btn.innerText="Deleteall";
+        appended.append(btn);
     }
   
 
@@ -93,15 +205,25 @@ const Edit=(id)=>{
       
       console.log(id);
   }
-  
+//   ---------------here we have to delete item----------------
 const Delete=(index)=>{
 
-   DATA.slice(index,1);
+//    DATA.splice(index,1);
+   delete DATA[index-1];
    getdata1(DATA);
    
 }
 
-    }
+    
+// ------pagination formula ---------------
+
+
+const handelChange=(v)=>{
+      console.log(start,end)
+       start=10*(v-1);
+       end=10*v;
+ getdata1(DATA,start,end);
+}
 
 
 
